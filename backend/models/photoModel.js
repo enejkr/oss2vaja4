8ff8f79eb2b7743eac771fe96a924ed4101f1aa7
enejkr <enejkr@gmail.com>
2025-05-04 10:05:56 +0200

    wdd

---
+++ backend/models/photoModel.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema;
+
+var photoSchema = new Schema({
+	'name' : String,
+	'description' : String,
+	'path' : String,
+	'postedBy' : {
+	 	type: Schema.Types.ObjectId,
+	 	ref: 'user'
+	},
+	'views' : Number,
+	'likes' : Number
+});
+
+module.exports = mongoose.model('photo', photoSchema);
